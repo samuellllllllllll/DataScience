c6bfbdab63aa677d0d5e88c48843e57b0ebf889e
Samuel Jarjanette <samuel.jarjanette@viacesi.fr>
2024-12-09 09:13:53 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mon_projet",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Liste des dépendances
+    ],
+    author="Votre Nom",
+    author_email="votre@email.com",
+    description="Description de votre projet",
+) 
